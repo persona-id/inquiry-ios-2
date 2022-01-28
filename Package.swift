a533fc9d75e0e613bdc6132251b6844baae39600
bowenwp <54548588+bowenwp@users.noreply.github.com>
2022-01-27 20:47:14 -0800

    Update Package.swift for 2.2.0

--- Package.swift
+++ Package.swift
@@ -1,1 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
 
+let package = Package(
+  name: "PersonaInquirySDK2",
+  platforms: [.iOS(.v11)],
+  products: [
+    .library(
+      name: "PersonaInquirySDK2",
+      targets: ["Persona2"]
+    )
+  ],
+  targets: [
+    .binaryTarget(
+      name: "Persona2",
+      url: "https://github.com/persona-id/inquiry-ios-2/releases/download/2.2.0/Persona2.xcframework.zip",
+      checksum: "9c42fac613896e43ec046a419c0cd9e706c21a4bfe110313ebe5336e058b505d"
+    )
+  ]
+)
