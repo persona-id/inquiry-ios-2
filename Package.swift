2e9a5eb96f65af2e511c2e3506185886c3f04e92
persona-identities <57889169+persona-identities@users.noreply.github.com>
2022-08-03 15:02:34 -0700

    Update Package.swift for 2.3.6

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Persona2",
-      url: "https://github.com/persona-id/inquiry-ios-2/releases/download/2.3.5/Persona2.xcframework.zip",
-      checksum: "c14a3d17404c8ea3fed7fbc98e3860116de8aea621f047129c657ff703ef8945"
+      url: "https://github.com/persona-id/inquiry-ios-2/releases/download/2.3.6/Persona2.xcframework.zip",
+      checksum: "cbda96dfaf9c4318d080a2f72d91f71201428352c514bd9787a0bce16bd3ccfa"
     )
   ]
 )
