455841ac6906b7f826f3c0fdf1afd4057ff17def
Ariel-Liao <46766415+Ariel-Liao@users.noreply.github.com>
2024-04-16 16:19:43 -0400

    Update Package.swift for 2.19.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .binaryTarget(
       name: "Persona2",
       url: "https://github.com/persona-id/inquiry-ios-2/releases/download/2.19.1/Persona2.xcframework.zip",
-      checksum: "21cd5fec2e033c7b4bb40609167fa94440f6ad8f99b67d045e17003e45c71dfc"
+      checksum: "576739c6a67fc7d1dd0802b48bd15e498e69281cabe76830c79f711e36367d6b"
     )
   ]
 )
