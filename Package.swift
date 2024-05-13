7a0cfb3e8c467004df3947f751147293b8ccd3d8
ariel-ariel-liao <142854114+ariel-ariel-liao@users.noreply.github.com>
2024-05-13 15:21:14 -0700

    Update Package.swift for 2.20.2

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     .binaryTarget(
       name: "Persona2",
       url: "https://github.com/persona-id/inquiry-ios-2/releases/download/2.20.2/Persona2.xcframework.zip",
-      checksum: "c600cd44d80edaadfb8d8bec7add79ebaa00f0df616f686b74d10cccbd554e59"
+      checksum: "63171c772e82d242502c0ead6e010f6f7dc16a7ff4c6988cb22a4f3bbb1573ae"
     )
   ]
 )
