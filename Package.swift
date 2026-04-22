07f7a83da6e4340e7c253f7f49a0667751b5eac7
mobile-build-persona <mobile-build@withpersona.com>
2026-04-22 10:24:59 -0600

    Update Package.swift for 2.45.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Persona2",
-      url: "https://github.com/persona-id/inquiry-ios-2/releases/download/2.45.0-RC/Persona2.xcframework.zip",
+      url: "https://github.com/persona-id/inquiry-ios-2/releases/download/2.45.0/Persona2.xcframework.zip",
       checksum: "3ce569e96d3f876ff1277c59dcb1c8c39cf0f841cf56725fbb5bb1f93fc26c8e"
     )
   ]
