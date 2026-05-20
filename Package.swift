b9fab95df6c86055d1a76e9679e08002405d7cc3
mobile-build-persona <mobile-build@withpersona.com>
2026-05-19 19:16:49 -0600

    Update Package.swift for 2.48.0-RC

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Persona2",
-      url: "https://storage.googleapis.com/sdk.withpersona.com/ios/v2/releases/2.48.0-beta.1/Persona2.xcframework.zip",
-      checksum: "0d0b660701ab188a46013b8f9e569ae46bf0fd469443cb48de9b781faed6f704"
+      url: "https://github.com/persona-id/inquiry-ios-2/releases/download/2.48.0-RC/Persona2.xcframework.zip",
+      checksum: "514de59109306bfdb9f6b1714efae9bb878f7ee389ca5c6f9ce612a1ddc1dd73"
     )
   ]
 )
