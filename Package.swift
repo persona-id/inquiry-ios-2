193648cb8062c4efedb631ef86195b8ef2d0103c
mobile-build-persona <mobile-build@withpersona.com>
2026-07-16 17:14:27 -0400

    Update Package.swift for 3.5.0-RC

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "PersonaInquirySDK2",
-  platforms: [.iOS(.v13)],
+  platforms: [.iOS("15.0")],
   products: [
     .library(
       name: "PersonaInquirySDK2",
@@ -13,8 +13,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "Persona2",
-      url: "https://github.com/persona-id/inquiry-ios-2/releases/download/2.51.1/Persona2.xcframework.zip",
-      checksum: "4f996d69c2875e9a2e8d62d7456029e56d1f225252799f8bf90f989df80f796e"
+      url: "https://github.com/persona-id/inquiry-ios-2/releases/download/3.5.0-RC/Persona2.xcframework.zip",
+      checksum: "1005594eeaa3478f9f516cb2fa43d342fb9b6b432229fa378900a25f7bdd833d"
     )
   ]
 )
