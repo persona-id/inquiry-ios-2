e1121658b8df1b0e6916976c2b6172a916d1b95b
mobile-build-persona <mobile-build@withpersona.com>
2026-08-17 16:54:21 -0400

    Update Package.swift for 2.52.2-RC

--- Package.swift
+++ Package.swift
@@ -3,32 +3,18 @@ import PackageDescription
 
 let package = Package(
   name: "PersonaInquirySDK2",
-  platforms: [.iOS("15.0")],
+  platforms: [.iOS(.v13)],
   products: [
     .library(
       name: "PersonaInquirySDK2",
       targets: ["Persona2"]
-    ),
-    .library(
-      name: "PersonaInquirySDK2Protected",
-      targets: ["Persona2Protected", "Persona2libloader"]
     )
   ],
   targets: [
     .binaryTarget(
       name: "Persona2",
-      url: "https://storage.googleapis.com/sdk.withpersona.com/ios/v2/releases/3.6.1-beta.3/Persona2.xcframework.zip",
-      checksum: "dc8a3c3cc034d1daedf02a0a1132c0b8da550d78bbcdf4cf13df7cb626ae73dd"
-    ),
-    .binaryTarget(
-      name: "Persona2Protected",
-      url: "https://storage.googleapis.com/sdk.withpersona.com/ios/v2/releases/3.6.1-beta.3/Persona2Protected.xcframework.zip",
-      checksum: "964e4d0160ec27e22ee250885de259331a38edc36b45703bee7e63f893630cb3"
-    ),
-    .binaryTarget(
-      name: "Persona2libloader",
-      url: "https://storage.googleapis.com/sdk.withpersona.com/ios/v2/releases/3.6.1-beta.3/Persona2libloader.xcframework.zip",
-      checksum: "218a143a769857306f82951df693f612b10eb1eeb9f786e12be4cfa45b278e3e"
+      url: "https://github.com/persona-id/inquiry-ios-2/releases/download/2.52.2-RC/Persona2.xcframework.zip",
+      checksum: "40cf8d7317784035f17ecf624da70cc00bc65aff8d4225237b75e35e1675e151"
     )
   ]
 )
